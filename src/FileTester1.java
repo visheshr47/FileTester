2ced0524bb0944f08b73b56887995fc0b299d4d0
visheshr47 <visheshr47@gmail.com>
2018-07-03 11:56:20 +0530

    First commit

---
+++ src/FileTester1.java
@@ -0,0 +1,14 @@
+import java.io.FileInputStream;
+
+public class FileTester1 {
+        public static void main(String args[]){
+            try{
+                FileInputStream fin=new FileInputStream("D:\\testout.txt");
+                int i=fin.read();
+                System.out.print((char)i);
+
+                fin.close();
+            }catch(Exception e){System.out.println(e);}
+        }
+    }
+
---
+++ src/FileTester2.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+
+public class FileTester2 {
+    public static void main(String args[]) {
+        try (FileInputStream fin = new FileInputStream("C:\\Users\\Dell pc\\Desktop\\rr.txt")) {
+            int i = fin.read();
+            System.out.print((char) i);
+
+            //        fin.close();
+              }catch(Exception e){System.out.println(e);}
+            //   }
+//}
+        }
+    }
+
+
